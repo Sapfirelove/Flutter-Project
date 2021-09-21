c2721dc009e69a7cc12a92362172a7f7da76baa5
Sapfirelove <schiebelm1850@gmail.com>
2021-09-20 21:04:30 -0400

    updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 void main() {}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text(),):
+  }
+}
